f72053ffcfc56b960bc14116ab16e8e936bfb70c
Roymand <chuyirlee@live.com>
2023-08-07 05:33:57 -0500

    added javascript so search bar creates a search history and updates 'City Name' on the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const apikey = "2b265a8a3022c8ba7b8d4c9a7af405b2"
-var city = ""
+var city = "Minneapolis"
 
 const weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
 
@@ -20,3 +20,38 @@ fetch(weatherurl) .then(response => {
     console.error('Fetch error:', error);
 })
 
+const searchBarButton = document.getElementById('searchButton')
+
+searchBarButton.addEventListener('click', () => {
+    const searchBar = document.getElementById('searchBar')
+    const historyDiv = document.getElementById('history')
+    const searchedCityName = searchBar.value;
+    const cityNameSpan = document.querySelectorAll('.cityName')
+
+    if (searchedCityName !== ''){
+        // Create the button
+        const historyButton = document.createElement('button')
+        historyButton.textContent = searchedCityName;
+        historyButton.className = 'col-10 m-auto rounded'
+
+        // Append to div
+        historyDiv.appendChild(historyButton);
+
+        // Add event listener for newly made button to change selected city
+        const buttonText = historyButton.textContent
+        historyButton.addEventListener('click', () => {
+             cityNameSpan.forEach(span => {
+            span.textContent = buttonText;
+        })
+        })
+
+        // Update cards with city name
+        cityNameSpan.forEach(span => {
+            span.textContent = historyButton.textContent;
+        })
+
+    }
+
+    // Reset search bar
+    searchBar.value = '';
+})
