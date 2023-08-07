93f315378f18794765ba3a55818db9ca00901b34
Roymand <chuyirlee@live.com>
2023-08-07 17:57:29 -0500

    Overhauled the javascript so it was using weather forecast instead of current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,64 @@
 const apikey = "2b265a8a3022c8ba7b8d4c9a7af405b2"
 var city = "Minneapolis"
+const cityNameSpan = document.getElementById('cityName')
 
-const weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+// Function to update weather forecast data
+function updateWeatherData(data) {
+    const forecasts = data.list; // Array of forecast data
 
-fetch(weatherurl) .then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok: ${response.status}');
+    // Update the content of the spans with the forecast data
+    for (let i = 0; i < forecasts.length; i++) {
+        const forecast = forecasts[i];
+        const temp = forecast.main.temp;
+        const humid = forecast.main.humidity;
+        const wind = forecast.wind.speed;
+        
+        const tempSpan = document.getElementById(`Temp${i}`);
+        const humidSpan = document.getElementById(`Humidity${i}`);
+        const windSpan = document.getElementById(`Wind${i}`);
+        cityNameSpan.textContent = city;
+
+        tempSpan.textContent = ` ${temp} °F`;
+        humidSpan.textContent = ` ${humid} %`;
+        windSpan.textContent = ` ${wind} mph`;
     }
-    return response.json();
-}) .then(data => {
-    var temp = data.main.temp;
-    var humid = data.main.humidity;
-    var wind = data.wind.speed;
+}
 
-    console.log('Temperature:', temp, 'C')
-    console.log('Humidity:', humid, '%')
-    console.log('Wind:', wind, 'm/s')
-}) .catch(error => {
-    console.error('Fetch error:', error);
-})
+// Function to fetch the data and call upon above function
+function fetchWeatherData(city) {
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=imperial`;
+
+    fetch(forecastUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            updateWeatherData(data);
+            console.log(forecasts)
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
+
+// Call the function initially
+fetchWeatherData(city);
 
 const searchBarButton = document.getElementById('searchButton')
 
 searchBarButton.addEventListener('click', () => {
     const searchBar = document.getElementById('searchBar')
     const historyDiv = document.getElementById('history')
-    const searchedCityName = searchBar.value;
-    const cityNameSpan = document.querySelectorAll('.cityName')
+    const searchedCityName = searchBar.value.trim();
 
     if (searchedCityName !== ''){
+        // Update city variable and fetch new weather data
+        city = searchedCityName;
+        fetchWeatherData(city)
+
         // Create the button
         const historyButton = document.createElement('button')
         historyButton.textContent = searchedCityName;
@@ -40,15 +70,13 @@ searchBarButton.addEventListener('click', () => {
         // Add event listener for newly made button to change selected city
         const buttonText = historyButton.textContent
         historyButton.addEventListener('click', () => {
-             cityNameSpan.forEach(span => {
-            span.textContent = buttonText;
-        })
+            cityNameSpan.textContent = buttonText
+            city = buttonText
+            fetchWeatherData(city)
         })
 
         // Update cards with city name
-        cityNameSpan.forEach(span => {
-            span.textContent = historyButton.textContent;
-        })
+        cityNameSpan.textContent = historyButton.textContent;
 
     }
 
